2f5b69f8f386a68fa9f45cad07ee32a1435cddd4
Balakrishna Kumar V <balakrishnakumar.v@gmail.com>
2022-04-24 21:07:20 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps("Hello from Lambda - CICD Pipeline. Source Github !!!!")
+        'body': json.dumps("Hello from Lambda ~~> CICD Pipeline. Source Github !!!!!")
     }
